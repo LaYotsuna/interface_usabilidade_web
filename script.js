c0afb6951875b7231dae969d852d36328583f380
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-14 22:10:20 -0300

    Finalização da parte de layout geral

--- script.js
+++ script.js
@@ -3,3 +3,8 @@
 import { someFunction } from "./module.js";
 
 someFunction("This is a module function");
+
+document.addEventListener("DOMContentLoaded", function () {
+  var navbarHeight = document.querySelector(".navbar").offsetHeight;
+  document.querySelector(".hero").style.paddingTop = navbarHeight + "px";
+});
