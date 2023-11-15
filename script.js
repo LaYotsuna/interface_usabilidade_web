2fbbc9aa9e78925727c1c06fde225fafabfadb91
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-15 00:46:10 -0300

    Código com lógica incorreta

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ document.getElementById("exploreButton").addEventListener("click", function () {
   document.getElementById("interface").scrollIntoView();
 });
 
+/* Numeração do slide */
+let slideCounter = 0;
+
 /* Slides da Interface */
 const slides = {
   slide1: [
@@ -34,10 +37,22 @@ const setas = [setaAnterior, setaProximo];
 function selecionarSeta() {
   setas.forEach((seta) => {
     seta.addEventListener("click", () => {
-      if (seta === setas[0]) {
-        alert("Você pressionou a seta anterior");
+      if (seta === setas[0] && slideCounter === 0) {
+        return null;
+      } else if (
+        (seta === setas[0] && slideCounter === 1) ||
+        (seta === setas[0] && slideCounter === 2)
+      ) {
+        slideCounter -= 1;
+        voltarSlide(slideCounter);
+      } else if (
+        (seta === setas[1] && slideCounter === 0) ||
+        (seta === setas[1] && slideCounter === 1)
+      ) {
+        slideCounter += 1;
+        passarSlide(slideCounter);
       } else {
-        alert("Você pressionou a seta próxima");
+        return null;
       }
     });
   });
@@ -59,5 +74,47 @@ function sliderDots() {
   });
 }
 
+function voltarSlide(num) {
+  if (num === 1) {
+    slides.slide1[0].classList.remove("oculto");
+    slides.slide1[1].classList.remove("oculto");
+    slides.slide1[0].classList.add("oculto");
+    slides.slide1[1].classList.add("oculto");
+    slides.slide2[0].classList.add("oculto");
+    slides.slide2[1].classList.add("oculto");
+  } else if (num === 2) {
+    slides.slide1[0].classList.add("oculto");
+    slides.slide1[1].classList.add("oculto");
+    slides.slide2[0].classList.remove("oculto");
+    slides.slide2[1].classList.remove("oculto");
+    slides.slide3[0].classList.add("oculto");
+    slides.slide3[1].classList.add("oculto");
+  }
+}
+
+function passarSlide(num) {
+  if (num === 0) {
+    slides.slide2[0].classList.add("oculto");
+    slides.slide2[1].classList.add("oculto");
+    slides.slide3[0].classList.add("oculto");
+    slides.slide3[1].classList.add("oculto");
+  } else if (num === 1) {
+    slides.slide1[0].classList.add("oculto");
+    slides.slide1[1].classList.add("oculto");
+    slides.slide2[0].classList.remove("oculto");
+    slides.slide2[1].classList.remove("oculto");
+    slides.slide3[0].classList.add("oculto");
+    slides.slide3[1].classList.add("oculto");
+  } else if (num === 2) {
+    slides.slide1[0].classList.add("oculto");
+    slides.slide1[1].classList.add("oculto");
+    slides.slide2[0].classList.add("oculto");
+    slides.slide2[1].classList.add("oculto");
+    slides.slide3[0].classList.remove("oculto");
+    slides.slide3[1].classList.remove("oculto");
+  }
+}
+
+passarSlide(slideCounter);
 selecionarSeta();
 sliderDots();
