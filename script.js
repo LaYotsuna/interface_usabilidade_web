735c649d1273e70fe33bdd7f510318f3d6d40a18
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-14 22:26:50 -0300

    ...

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-import { someFunction } from "./module.js";
-
-someFunction("This is a module function");
-
 document.addEventListener("DOMContentLoaded", function () {
   var navbarHeight = document.querySelector(".navbar").offsetHeight;
   document.querySelector(".hero").style.paddingTop = navbarHeight + "px";
