eee17b236ed10549c22d841bf65ad96bd2cfffc3
Larissa Cerqueira <larissacerqueirayt@gmail.com>
2023-11-14 22:42:18 -0300

    Adicionado a função de scroll do botão explorar

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var navbarHeight = document.querySelector(".navbar").offsetHeight;
   document.querySelector(".hero").style.paddingTop = navbarHeight + "px";
 });
+
+document.getElementById("exploreButton").addEventListener("click", function () {
+  document.getElementById("interface").scrollIntoView();
+});
